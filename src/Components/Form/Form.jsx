0a8d023ae015850322dd3c89c3a036643319bf40
PabloNotEscobar <0kvestik0@gmail.com>
2025-05-01 22:31:21 +0300

    init

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = () => {
+
+    const [country, setCountry] = useState('')
+    const [city, setCity] = useState('')
+    const [subject, setSubject] = useState('physical')
+
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+
+    const onChangeCity = (e) => {
+        setCity(e.target.value)
+    }
+
+
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
+
     return (
         <div>
-            Form
+            <h3>Введите ваши данные</h3>
+            <input
+                className='input'
+                value={country}
+                type="text"
+                placeholder="Страна"
+                onChange={onChangeCountry}
+            />
+            <input
+                className='input'
+                type="text"
+                value={city}
+                placeholder="Улица"
+                onChange={onChangeCity}
+            />
+            <select value={subject} onChange={onChangeSubject} className='select'>
+                <option value='physical'>Физ. лицо</option>
+                <option value='legal'>Юр. лицо</option>
+            </select>
         </div>
     );
 };
