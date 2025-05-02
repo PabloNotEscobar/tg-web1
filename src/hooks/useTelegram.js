ce0337a3f9eee4dab48625b103c094bed545b98e
PabloNotEscobar <0kvestik0@gmail.com>
2025-05-02 14:41:51 +0300

    init

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,31 +1,24 @@
-
+const tg = window.Telegram.WebApp;
 
 export function useTelegram() {
-    const tg = window.Telegram?.WebApp;
-    if (!tg) {
-        console.error("Telegram WebApp not initialized!");
-        return { tg: null };
-    }
-
 
     const onClose = () => {
         tg.close()
     }
 
-
     const onToggleButton = () => {
         if(tg.MainButton.isVisible) {
-            tg.MainButton.hide()
+            tg.MainButton.hide();
         } else {
-            tg.MainButton.show()
+            tg.MainButton.show();
         }
     }
 
-
     return {
         onClose,
+        onToggleButton,
         tg,
         user: tg.initDataUnsafe?.user,
-        onToggleButton
+        queryId: tg.initDataUnsafe?.query_id,
     }
 }
