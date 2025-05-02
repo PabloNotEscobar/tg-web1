39cddc35f368a89cfd29319b184d16278283ef30
PabloNotEscobar <0kvestik0@gmail.com>
2025-05-02 15:26:29 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import Header from "./Components/Header/Header";
+import {Route, Routes} from "react-router-dom";
+import ProductList from "./Components/ProductList/ProductList";
 import Form from "./Components/Form/Form";
 
-function App() {
-    return <Form />;
-}
 
+function App() {
+
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'/'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
+}
+//
 export default App;
