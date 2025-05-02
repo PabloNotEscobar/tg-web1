9feb88c838129271bf9db5235b9b4957ae8164ca
PabloNotEscobar <0kvestik0@gmail.com>
2025-05-02 15:50:05 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useTelegram } from './hooks/useTelegram';
+import Header from './components/Header/Header';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 import './App.css';
-import {useEffect} from "react";
-import Header from "./Components/Header/Header";
-import {useTelegram} from "./hooks/useTelegram";
-import {Route, Routes} from "react-router-dom";
-import ProductList from "./Components/ProductList/ProductList";
-import Form from "./Components/Form/Form";
-
 
 function App() {
-    const { tg } = useTelegram()
+    const { tg } = useTelegram(); // Деструктуризация корректно
 
     useEffect(() => {
-        tg.ready()
-    }, [tg])
-
+        if (tg) {
+            tg.ready(); // Инициализация WebApp
+            tg.expand(); // Раскрыть на весь экран
+        }
+    }, [tg]);
 
     return (
         <div className="App">
-            <Header/>
+            <Header />
             <Routes>
-                <Route index element={<ProductList />}/>
-                <Route path="form" element={<Form />}/>
+                <Route index element={<ProductList />} />
+                <Route path="form" element={<Form />} />
             </Routes>
         </div>
     );
 }
-//
+
 export default App;
