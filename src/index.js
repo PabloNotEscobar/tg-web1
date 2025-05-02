ba95830e74e2683b0c5f4ea1e9905022f12821de
PabloNotEscobar <0kvestik0@gmail.com>
2025-05-02 13:00:48 +0300

    init

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-if (window.Telegram?.WebApp) {
-    window.Telegram.WebApp.ready(); // Сообщаем Telegram, что WebApp готов
-    window.Telegram.WebApp.expand(); // Раскрываем на весь экран
-}
 root.render(
 
     <React.StrictMode>
