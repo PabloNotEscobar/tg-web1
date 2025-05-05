00f250583a50b20c03ccfa2b5b75c7c75ed85468
PabloNotEscobar <0kvestik0@gmail.com>
2025-05-05 18:54:21 +0300

    init

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -32,7 +32,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId
         }
-        fetch('http://localhost:8000', {
+        fetch('https://tg-web-bck-shop-3.onrender.com', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
