26e8917e15c1d09f75ea7d97bd619fd8f85610c5
PabloNotEscobar <0kvestik0@gmail.com>
2025-05-05 17:33:59 +0300

    init

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import ProductItem from "../ProductItem/ProductItem";
 import './ProductList.css'
 import {useTelegram} from "../../hooks/useTelegram";
@@ -23,7 +23,31 @@ const getTotalPrice = (items = []) => {
 const ProductList = () => {
 
     const [addedItems, setAddedItems] = useState([])
-    const { tg } = useTelegram()
+    const { tg, queryId} = useTelegram()
+
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId
+        }
+        fetch('http://localhost:8000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        tg.sendData(JSON.stringify(data))
+    }, [addedItems, queryId, tg])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData, tg, tg.WebApp]);
 
 
     const onAdd = (product) => {
