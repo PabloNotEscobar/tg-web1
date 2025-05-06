4c3eec05cffdac3ae3eb633769e52d9659cffcdc
PabloNotEscobar <0kvestik0@gmail.com>
2025-05-06 20:52:44 +0300

    init

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -39,6 +39,7 @@ const ProductList = () => {
             },
             body: JSON.stringify(data)
         })
+        console.log('бэк схватил')
     }, [addedItems, queryId])
 
     useEffect(() => {
